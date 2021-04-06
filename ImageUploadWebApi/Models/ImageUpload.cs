be828c205291a502ea3050f38521822ddce75d57
Nikos Salichos <niksal1991@gmail.com>
2021-04-06 12:34:26 +0300

    Add project files.

---
+++ ImageUploadWebApi/Models/ImageUpload.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace ImageUploadWebApi.Models
+{
+    public class ImageUpload
+    {
+        [Required(ErrorMessage = "Image upload is required, please select a file")]
+        public IFormFile Image { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+    }
+}
